50632a959c332523bbae4fdad4f49adf66eb32f1
Sp9nky <35467365+Sp9nky@users.noreply.github.com>
2022-01-23 08:14:20 +0200

    Add files via upload

---
+++ AutoAcceptLoL.py
@@ -0,0 +1,38 @@
+import pyautogui
+import time
+from winreg import *
+import sys, os
+import cv2
+
+def addStartup():  # this will add the file to the startup registry key
+    fp = os.path.dirname(os.path.realpath(__file__))
+    file_name = sys.argv[0].split('\\')[-1]
+    new_file_path = fp + '\\' + file_name
+    keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
+    key2change = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
+    SetValueEx(key2change, 'AutoAccceptLoL', 0, REG_SZ,
+               new_file_path)
+
+def Hide():
+    import win32console
+    import win32gui
+    win = win32console.GetConsoleWindow()
+    win32gui.ShowWindow(win, 0)
+
+addStartup()
+
+Hide()
+
+while True:
+    try:
+        acc = \
+            pyautogui.locateOnScreen(
+                'accept.PNG',
+                confidence=.7
+            )
+        acc_co = pyautogui.center(acc)
+        x,y = acc_co.x, acc_co.y
+        pyautogui.moveTo(x,y)
+        pyautogui.click(x,y)
+    except:
+        time.sleep(1)
